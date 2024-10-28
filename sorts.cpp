ce0c24f234cf74a093ca68143e16ea09c1680d68
Anna-Leb <banankaanna@gmail.com>
2024-10-29 00:51:50 +0500

    Добавлена сортировка перебором

--- sorts.cpp
+++ sorts.cpp
@@ -1,3 +1,59 @@
+//сортировка перебором
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+int find_smallest(int list[], int start_p, int list_len)
+{
+ int smallest_p = start_p;
+
+ for (int i = start_p; i < list_len; i++)
+ {
+  if (list[i] < list[smallest_p])
+   smallest_p = i;
+ }
+ return smallest_p;
+}
+
+void sel_sort(int list[], int list_len)
+{
+ for (int i = 0; i < list_len; i++)
+ {
+  int smallest_p = find_smallest(list, i, list_len);
+  swap(list[i], list[smallest_p]);
+ }
+ return;
+}
+
+int main()
+{
+ int list_len;
+ cout << "Введите длину массива: ";
+ cin >> list_len;
+
+ int* list = new int[list_len]; 
+
+ srand(time(NULL));  
+ for (int i = 0; i < list_len; i++) 
+ {
+    list[i] = rand() % 100;
+ }
+
+ cout << "Массив до сортировки: " << endl;
+ for (int i = 0; i < list_len; i++)
+    cout << list[i] << '\t';
+ cout << endl;
+
+ sel_sort(list, list_len);
+
+ cout << "Массив после сортировки: " << endl;
+ for (int i = 0; i < list_len; i++)
+    cout << list[i] << '\t';
+ cout << endl;
+}
+
+
 //сорировка вставками
 #include <iostream>
 #include <time.h>
